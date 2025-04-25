e1cccf2eeeb79b90e19907266fabfd58a98af226
Linus Heck <linus.heck@ru.nl>
2025-04-25 12:04:20 +0200

    lpl-standard-nr

--- benchmarks/lpl_vs_standard.sh
+++ benchmarks/lpl_vs_standard.sh
@@ -24,7 +24,7 @@ end
 
 echo $folder
 
-python3 generate_commands.py --folder testcases_standard --global-override benchmarks/lpl_vs_standard.json --storm-location ../storm/build_release/bin/ --output $folder --jobs $jobs --timeout $timeout
+python3 generate_commands.py --folder testcases --global-override benchmarks/lpl_vs_standard_all.json --storm-location ../storm/build_release/bin/ --output $folder --jobs $jobs --timeout $timeout
 ./$folder/parallel.sh
 
 set result_file $folder/results.csv
@@ -32,11 +32,11 @@ set result_file $folder/results.csv
 python3 process_output.py $folder/output/* --results $result_file
 
 # slowdown without enabling bigstep
-python csv_to_table.py $result_file --comp-field RobustPLA --comp-values "true,false" --filter "BigStep:false" --avg-slowdown > $folder/slowdown.txt
-python csv_to_table.py $result_file --comp-field RobustPLA --comp-values "false,true" --filter "BigStep:false" > $folder/table-slowdown.tex
+# python csv_to_table.py $result_file --comp-field RobustPLA --comp-values "true,false" --filter "BigStep:false" --avg-slowdown > $folder/slowdown.txt
+# python csv_to_table.py $result_file --comp-field RobustPLA --comp-values "false,true" --filter "BigStep:false" > $folder/table-slowdown.tex
 
 # comment pgf in for latex generation (requires texlive)
-for format in pdf #pgf
+for format in pdf pgf
     # slowdown without enabling bigstep
     python csv_to_scatter.py $result_file true false "lifted" "standard" --compare-by "Time (wall)" --comp-field "RobustPLA" --filter "BigStep:false;Epsilon:1e-05" --output-pdf $folder/"time-slowdown.$format" --separate-legend 1
     python csv_to_scatter.py $result_file true false "lifted" "standard" --compare-by "Regions" --comp-field "RobustPLA" --filter "BigStep:false;Epsilon:1e-05" --output-pdf $folder/"regions-slowdown.$format" --separate-legend 1

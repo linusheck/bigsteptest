80f98d134c88a2bfe3325db90e50ff2925889c6d
Linus Heck <linus.heck@ru.nl>
2025-04-25 12:55:08 +0200

    up

--- benchmarks/bigstep.sh
+++ benchmarks/bigstep.sh
@@ -30,13 +30,13 @@ python process_output.py $folder/output/* --results $result_file
 
 function plot --description "plot <format> <separate-legend> <dpi> <title>"
     for epsilon in 0.1 0.01 0.0001 1e-05
-        python csv_to_scatter.py $result_file true false "bigstep" "no bigstep" --compare-by "Time (wall)" --filter "Epsilon:$epsilon;Region Bound:0!0.1!1e-06!0,0.8!0.2,1" --output-pdf $folder/"time-$(string replace . , $epsilon).$argv[1]" --separate-legend $argv[2] --figsize $argv[3] --title $argv[4]
+        python csv_to_scatter.py $result_file true false "bigstep" "no bigstep" --compare-by "Time (wall)" --max 4 --filter "Epsilon:$epsilon;Region Bound:0!0.1!1e-06!0,0.8!0.2,1" --output-pdf $folder/"time-$(string replace . , $epsilon).$argv[1]" --separate-legend $argv[2] --figsize $argv[3] --title $argv[4]
         python csv_to_scatter.py $result_file true false "bigstep" "no bigstep" --compare-by "Regions" --min 0 --max 5 --filter "Epsilon:$epsilon;Region Bound:0!0.1!1e-06!0,0.8!0.2,1" --output-pdf $folder/"regions-$(string replace . , $epsilon).$argv[1]" --separate-legend $argv[2] --figsize $argv[3] --title $argv[4]
     end
 end
 
 plot pdf 0 7 1
 # comment in for latex generation (requires texlive)
-# plot pgf 1 6 0
+plot pgf 1 6 0
 
 python csv_to_table.py $result_file --comp-field BigStep --comp-values "false,true" > $folder/table.tex

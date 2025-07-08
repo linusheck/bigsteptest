193085043afbd43feea2b3e3d04db0bdd708ef03
Linus Heck <linus.heck@ru.nl>
2025-07-08 10:01:48 +0200

    Fix path

--- benchmarks/nonsimple.sh
+++ benchmarks/nonsimple.sh
@@ -4,4 +4,4 @@ set datetime (date "+%Y-%m-%d-%H-%M-%S")
 
 cd casestudies/nonsimple
 ./benchmark.sh > ../../experiments/q3-nonsimple-$datetime.log 2>&1
-python make_table.py ../../experiments/nonsimple-$datetime.log > ../../experiments/q3-nonsimple-$datetime-table.tex
+python make_table.py ../../experiments/q3-nonsimple-$datetime.log > ../../experiments/q3-nonsimple-$datetime-table.tex
